eabf72e469ca9210f857bf9633f58e4426b0f2e9
Mikita Tsiarentsyeu <Mikita_Tsiarentsyeu@epam.com>
2021-10-04 22:19:55 +0300

    Add lesson 04.10

---
+++ Lessons/04.10/io.py
@@ -0,0 +1,22 @@
+print("hello world!")
+
+x = 5
+print(x)
+
+y = 8.8
+z = 22
+print(x, y, z, "some another value", x, y, z)
+
+print(x, y, z, sep=', ', end='.\n')
+print(x, y, z, sep=', ', end='')
+print("test")
+
+x = input()
+print(x)
+
+y = input("Please input your data:\n")
+print(y)
+
+z = input("Enter some number: ")
+z = int(z)
+print(type(z))
---
+++ Lessons/04.10/test.py
@@ -0,0 +1,6 @@
+print("test")
+
+print("354" + 3.5)
+# print(3/0)
+
+print("test")
