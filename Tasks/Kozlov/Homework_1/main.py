2d19be69cd6595246cc57ab398c763159ad1b860
Algerdi <82950883+Algerdi@users.noreply.github.com>
2021-10-06 18:55:31 +0300

    Moved the function higher

--- Tasks/Kozlov/Homework_1/main.py
+++ Tasks/Kozlov/Homework_1/main.py
@@ -4,14 +4,6 @@ RUBLI = ['рубль', 'рубля', 'рублей']
 COINS = ['копейка', 'копейки', 'копеек']
 period, percent, amount = [int(input(f'Введите {INPUT[i]}:\n')) for i in range(3)]
 
-# Calculation of the final amount
-mon_perc = percent / 1200
-mon_peri = int(period * 12)
-for _ in range(mon_peri):
-    amount *= (1 + mon_perc)
-x = int(amount // 1)
-y = int(100*(round(amount % 1, 2)))
-
 
 # The function of setting the correct ending of words
 def vvod(tom, value):
@@ -26,6 +18,15 @@ def vvod(tom, value):
         return f'{value} {t[1]}'
     else:
         return f'{value} {t[2]}'
+    
+    
+# Calculation of the final amount
+mon_perc = percent / 1200
+mon_peri = int(period * 12)
+for _ in range(mon_peri):
+    amount *= (1 + mon_perc)
+x = int(amount // 1)
+y = int(100*(round(amount % 1, 2)))
 
 
 print(f'Сумма вашего депозита на конец срока составит {vvod("rub", x)} и {vvod("coin", y)}.')
