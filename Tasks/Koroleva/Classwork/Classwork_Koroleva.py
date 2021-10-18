00d1fc40f019eeec259d4be3d67cf66b78421ae5
KaralevaK <katsiaryna.karaleva@gmail.com>
2021-10-18 20:54:07 +0300

    Update Classwork_Koroleva.py

--- Tasks/Koroleva/Classwork/Classwork_Koroleva.py
+++ Tasks/Koroleva/Classwork/Classwork_Koroleva.py
@@ -1,33 +1,31 @@
 #строка числительных
 
-dict = {1:"one",
-        2:"two",
-        3:"three",
-        4:"four",
-        5:"five",
-        6:"six",
-        7:"seven",
-        8:"eight",
-        9:"nine",
-        10:"ten",
-        11:"eleven",
-        12:"twelve",
-        13:"thirteen",
-        14:"fourteen",
-        15:"fifteen",
-        16:"sixteen",
-        17:"seventeen",
-        18:"eighteen",
-        19:"nineteen",
-        20:"twenty"
+dict = {"one":1,
+        "two":2,
+        "three":3,
+        "four":4,
+        "five":5,
+        "six":6,
+        "seven":7,
+        "eight":8,
+        "nine":9,
+        "ten":10,
+        "eleven":11,
+        "twelve":12,
+        "thirteen":13,
+        "fourteen":14,
+        "fifteen":15,
+        "sixteen":16,
+        "seventeen":17,
+        "eighteen":18,
+        "nineteen":19,
+        "twenty":20
        }
 
 l = input("Введите строку, содержащую числительные больше 0 и меньше 21 через пробел:").split(" ")
 print(f"Строка, преобразованная в коллекцию:\n{l}")
 for i in range(len(l)):
-    for k,v in dict.items():
-        if l[i] == v:
-            l[i] = k     
+    l[i] = dict[l[i]]     
 print(f"Строка, преобразованная в числа:\n{l}")
 print(f"Строка без дубликатов:\n{set(l)}")
 l=sorted(list(set(l)))
