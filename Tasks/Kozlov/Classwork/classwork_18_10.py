5d98a157a9dfe1574571bbc29cff26cadf7cf90e
Algerdi <kzlv.vldslv@gmail.com>
2021-10-19 18:33:24 +0300

    Updated classwork_1

--- Tasks/Kozlov/Classwork/classwork_18_10.py
+++ Tasks/Kozlov/Classwork/classwork_18_10.py
@@ -2,24 +2,29 @@ dic_num = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seve
            'eleven': 11, 'twelve': 12, 'thirteen': 13, 'fourteen': 14, 'fifteen': 15, 'sixteen': 16, 'seventeen': 17,
            'eighteen': 18, 'nineteen': 19, 'twenty': 20}
 # data_input = 'five thirteen two eleven seventeen two one thirteen ten four eight five nineteen'
-data_input = input('Input your dic:\n').split()
-print(data_input)
+data_input = list(set(input('Input your string:\n').split()))
 
-print('Task 1:\n')
-data_input = [dic_num[i] for i in data_input]
-print(data_input)
+data_input.insert(0, [0, 0, 0, 0])
 
-print('Task 2 and 3:\n')
-data_input = list(set(data_input))
-print(data_input)
+print('Sum and product of numbers:')
+for i in dic_num:
+    if i in data_input[1:]:
+        data_input.append(dic_num[data_input.pop(data_input.index(i))])      # Extract the text and add a number.
 
-print('Task 4:\n')
-for i in range(len(data_input)-1):
-    if i % 2 == 0:
-        print(data_input[i]*data_input[i+1], end=' ')
-    else:
-        print(data_input[i] + data_input[i + 1], end=' ')
+        data_input[0][1], data_input[0][2] = data_input[0][2], dic_num[i]    # Numbers that we will perform operations on.
+
+        if type(data_input[-1]) == int and type(data_input[-2]) == int:
+            if data_input[0][3] == 0:
+                print(data_input[0][1] * data_input[0][2], end=' ')          # Output the results of operations.
+                data_input[0][3] = 1
+            else:
+                print(data_input[0][1] + data_input[0][2], end=' ')          # Output the results of operations.
+                data_input[0][3] = 0
+
+        if dic_num[i] % 2 != 0:
+            data_input[0][0] += dic_num[i]                                   # Sum of odd numbers.
 print()
+print('Ordered list:', data_input[1:], sep='\n')
+print('Sum of odd numbers:', data_input[0][0], sep='\n')
 
-print('Task 5:\n')
-print(sum([i for i in data_input if i % 2 != 0]))
+
